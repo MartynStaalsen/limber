fc854ddfe12083e811834c198ffd35299156e7af
Martyn Staalsen <martynstaalsen@gmail.com>
2026-03-11 15:14:08 -0500

    p1: convert signal impls to macro

--- lib/src/objs/signal.rs
+++ lib/src/objs/signal.rs
@@ -7,55 +7,27 @@ pub enum Value {
 pub trait SignalType: Into<Value> + TryFrom<Value> {
 }
 
-impl From<bool> for Value { // implements Into for SignalType
-    fn from(val: bool) -> Value {
-        Value::Bool(val)
-    }
-}
-impl TryFrom<Value> for bool{
-    type Error = &'static str;
-
-    fn try_from(val: Value) -> Result<Self, Self::Error> {
-        match val {
-            Value::Bool(v) => Ok(v),
-            _ => Err("Value was not Bool")
+macro_rules! map_signal_types {
+    ($t:ty, $variant:ident) => {
+        impl From<$t> for Value { // implements Into for SignalType
+            fn from(val: $t) -> Value {
+                Value::$variant(val)
+            }
         }
-    }
-}
-impl SignalType for bool {}
+        impl TryFrom<Value> for $t{
+            type Error = &'static str;
 
-impl TryFrom<Value> for i32{
-    type Error = &'static str;
-
-    fn try_from(val: Value) -> Result<Self, Self::Error> {
-        match val {
-            Value::Int(i) => Ok(i),
-            _ => Err("Value was not an Int")
+            fn try_from(val: Value) -> Result<Self, Self::Error> {
+                match val {
+                    Value::$variant(v) => Ok(v),
+                    _ => Err("Value was incorrect variant")
+                }
+            }
         }
+        impl SignalType for $t {}
     }
 }
-impl From<i32> for Value {
-    fn from(val: i32) -> Value {
-        Value::Int(val)
-    }
-}
-impl SignalType for i32 {}
 
-
-impl TryFrom<Value> for f32{
-    type Error = &'static str;
-
-    fn try_from(val: Value) -> Result<Self, Self::Error> {
-        match val {
-            Value::Float(f) => Ok(f),
-            _ => Err("Value was not a Float")
-        }
-    }
-}
-impl From<f32> for Value {
-    fn from(val: f32) -> Value {
-        Value::Float(val)
-    }
-}
-impl SignalType for f32 {}
-
+map_signal_types!(bool, Bool);                                                                                                              
+map_signal_types!(i32, Int);                                                                                                                
+map_signal_types!(f32, Float);  
